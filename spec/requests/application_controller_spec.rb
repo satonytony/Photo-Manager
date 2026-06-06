f2ca4e2a8cc5ecd5c4fb784188e6846f25f23263
satorytory <satoshinakaom044de+github@gmail.com>
2026-06-06 09:29:49 +0900

    ApplicationControllerのspecを補完: require_loginとcurrent_userのケースを追加

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -1,8 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :request do
+  let(:user) { create(:user) }
+
   describe "#require_login" do
     context "未ログイン状態でアクセスしたとき" do
+      it "ログイン画面にリダイレクトする" do
+        get photos_path
+        expect(response).to redirect_to(login_path)
+      end
+    end
+
+    context "ログイン済みのとき" do
+      before { post login_path, params: { email: user.email, password: "password" } }
+
+      it "リダイレクトしない" do
+        get photos_path
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "#current_user" do
+    context "ログイン後にセッションが有効なとき" do
+      before { post login_path, params: { email: user.email, password: "password" } }
+
+      it "リクエスト間でユーザーが維持される" do
+        get photos_path
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "ログイン後にユーザーが削除されたとき" do
+      before do
+        post login_path, params: { email: user.email, password: "password" }
+        user.destroy
+      end
+
       it "ログイン画面にリダイレクトする" do
         get photos_path
         expect(response).to redirect_to(login_path)
