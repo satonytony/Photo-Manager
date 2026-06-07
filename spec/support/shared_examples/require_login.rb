edafb474df6b3c7c94e7e03678eb44fe8a252743
satorytory <satoshinakaom044de+github@gmail.com>
2026-06-07 14:45:47 +0900

    shared_contextの名前をApplicationController継承確認に変更し、ログイン必須の担保を説明するコメントを追加

--- spec/support/shared_examples/require_login.rb
+++ spec/support/shared_examples/require_login.rb
@@ -1,6 +1,7 @@
-RSpec.shared_context "ログイン必須" do
+RSpec.shared_context "ApplicationController継承確認" do
   if described_class.is_a?(Class) && described_class < ApplicationController && described_class != SessionsController
     it "ApplicationController を継承している" do
+      # ApplicationController を継承していれば before_action :require_login が自動適用され、ログイン必須が担保される
       expect(described_class < ApplicationController).to be(true)
     end
   end
